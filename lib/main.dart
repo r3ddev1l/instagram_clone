54ea8df4befee80d0f18bb09ca01b2e48688c546
Srijan <srijanmaharjan1@gmail.com>
2022-01-25 13:53:27 +0545

    Firebase setup completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
 import 'package:instagram_clone/responsive/responsive_layout_screen.dart';
 import 'package:instagram_clone/responsive/web_screen_layout.dart';
 import 'package:instagram_clone/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyBH83DesJG1Y_UXf7nOJgtrWLVDQaizHD8",
+        appId: "1:1037854655614:web:0994da687ab4a071f7c42f",
+        messagingSenderId: "1037854655614",
+        projectId: "instagram-clone-85f00",
+        storageBucket: "instagram-clone-85f00.appspot.com",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
